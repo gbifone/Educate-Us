2aa8bd45bac7a145cb4a633ec80cddeb6fbf393a
tcioffidinkel <tcioffidinkel@gmail.com>
2017-02-19 19:04:35 -0800

    updated baseActivity

--- app/src/main/java/com/example/timothy/educate_us/BaseActivity.java
+++ app/src/main/java/com/example/timothy/educate_us/BaseActivity.java
@@ -57,19 +57,15 @@ public class BaseActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -82,11 +78,9 @@ public class BaseActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
             Intent intent = new Intent(this, HomeActivity.class);
             this.startActivity(intent);
             drawer.closeDrawers();
